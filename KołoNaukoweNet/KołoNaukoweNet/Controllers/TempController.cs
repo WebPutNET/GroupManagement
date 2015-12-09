20bc78727e698617e37d247eea29ca08da30e796
Mersey33 <konrad.dysput@gmail.com>
2015-12-09 12:52:04 +0100

    Added project with git ignore

---
+++ KołoNaukoweNet/KołoNaukoweNet/Controllers/TempController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KołoNaukoweNet.Controllers
+{
+    [RoutePrefix("PanelLogowania")]
+
+    public class TempController : Controller
+    {
+        [Route("start")] 
+        // GET: Temp 
+        public ActionResult Index()
+        {
+            return Content("siemanko");
+        }
+
+        public ActionResult GetParameters(string input)
+        {
+            return Content(input);
+        }
+    }
+}
